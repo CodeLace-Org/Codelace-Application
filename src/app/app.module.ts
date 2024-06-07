14269eb5fa2e5ef05c41752db44c69f4c0309424
LuisFelipePoma <u202110902@upc.edu.pe>
2024-06-06 23:23:39 -0500

    struct files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
-import { NavbarComponent } from './navbar/navbar.component'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 import { MaterialModule } from './material/material.module'
+import { NavbarComponent } from './shared/navbar/navbar.component'
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NavbarComponent],
