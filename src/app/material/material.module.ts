d440d28ca76122c6ef47f766d2b06119989d2569
Andre <andrepilcoch@gmail.com>
2024-06-08 22:11:41 -0500

    feat: implementación de la barra de navgación lateral responsiva (escritorio)

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { MatSidenavModule } from '@angular/material/sidenav'
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatSidenavModule
+  ],
   exports: [
-    // Muchachones agregen lo que necesiten aqui
+    MatSidenavModule
   ]
 })
 export class MaterialModule {}
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  public sideNavState: boolean = false;
+
+  ngOnInit() {}
 
 }
