48c33eaccaab43adae6b8705a5ded1a8b58e6fac
unknown <renzoec.u@gmail.com>
2024-06-23 00:02:38 -0500

    feat: profile edit implementation

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 const routes: Routes = [
-  { path: '', component: MyProfileComponent }
-
+  { path: '', component: MyProfileComponent },
+  { path: 'info', component: EditProfileComponent }
 ];
 
 @NgModule({
